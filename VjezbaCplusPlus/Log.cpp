339a39b6dfb336f1df4cdff8749e0c6d0c02f2e7
Borota Igor <borota.igor@gmail.com>
2020-05-23 10:42:53 +0200

    aaa

---
+++ VjezbaCplusPlus/Log.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void Log(const char* message)
+{
+	std::cout << "heya" << std::endl;
+}
--- VjezbaCplusPlus/VjezbaCplusPlus.cpp
+++ VjezbaCplusPlus/VjezbaCplusPlus.cpp
@@ -1,15 +1,9 @@
-// VjezbaCplusPlus.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include "pch.h"
 #include <iostream>
 
-void Log(const char* message) 
-{
-	std::cout << "heya" << std::endl;
-}
+void Log(const char* message);
 
 int main()
 {
+	Log("Hello world");
 	std::cin.get();
 }
