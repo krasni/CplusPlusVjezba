25aba5d3bf73fb978ec642bc21b4403192b7c094
Borota Igor <borota.igor@gmail.com>
2020-06-03 06:28:00 +0200

    log klasa

---
+++ VjezbaCplusPlus/src/VjezbaCplusPlus.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std:endl;
+
+class Log {
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo;
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[WARNING]: " << message << std::endl;
+	}
+
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+};
+
+int main()
+{
+	Log log;
+	log.SetLevel(log.LogLevelWarning);
+	log.Warn("Hello");
+	std::cin.get();
+}
