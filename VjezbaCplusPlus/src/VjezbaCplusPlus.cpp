9acea54fe133050080f5a0823904dd7968ecbba8
Borota Igor <borota.igor@gmail.com>
2020-06-14 21:56:12 +0200

    Radi se

--- VjezbaCplusPlus/src/VjezbaCplusPlus.cpp
+++ VjezbaCplusPlus/src/VjezbaCplusPlus.cpp
@@ -1,43 +1,106 @@
 #include <iostream>
+#include <string>
+#include <memory>
+#include <GLFW/glfw3.h>
+#include <vector>
+
 
 #define LOG(x) std::cout << x << std:endl;
 
 class Log {
 public:
-	const int LogLevelError = 0;
-	const int LogLevelWarning = 1;
-	const int LogLevelInfo = 2;
+	enum Level 
+	{
+		LevelError = 0, LevelWarning, LevelInfo
+	};
 private:
-	int m_LogLevel = LogLevelInfo;
+	Level m_LogLevel = LevelInfo;
 public:
-	void SetLevel(int level)
+	void SetLevel(Level level)
 	{
 		m_LogLevel = level;
 	}
 
 	void Info(const char* message)
 	{
-		if (m_LogLevel >= LogLevelInfo)
+		if (m_LogLevel >= LevelInfo)
 			std::cout << "[INFO]: " << message << std::endl;
 	}
 
 	void Warn(const char* message)
 	{
-		if (m_LogLevel >= LogLevelWarning)
+		if (m_LogLevel >= LevelWarning)
 			std::cout << "[WARNING]: " << message << std::endl;
 	}
 
 	void Error(const char* message)
 	{
-		if (m_LogLevel >= LogLevelError)
+		if (m_LogLevel >= LevelError)
 			std::cout << "[ERROR]: " << message << std::endl;
 	}
 };
 
+class Entity
+{
+public:
+	Entity()
+	{
+		std::cout << "Kreirano Entity!" << std::endl;
+	}
+
+	~Entity()
+	{
+		std::cout << "Destruktno Entity!" << std::endl;
+	}
+
+	void Print()
+	{
+			
+	}
+};
+
+struct Vertex
+{
+	float x, y, z;
+
+	Vertex(float x, float y, float z)
+		:x(x), y(y), z(z)
+	{
+	}
+
+	Vertex(const Vertex& vertex)
+		:x(x), y(y), z(z)
+	{
+		std::cout << "Copied!" << std::endl;
+	}
+
+	~Vertex()
+	{
+		//std::cout << "destroyed" << std::endl;
+	}
+};
+
+std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
+{
+	stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
+	return stream;
+}
+
 int main()
 {
-	Log log;
-	log.SetLevel(log.LogLevelWarning);
-	log.Warn("Hello");
+	//int a = glfwInit();
+	//std::cout << a << std::endl; 
+
+	//{
+	//	std::unique_ptr<Entity> entity = std::make_unique<Entity>();
+	//	entity->Print();
+	//}
+
+	std::vector<Vertex> vertices;
+	vertices.reserve(3);
+	vertices.emplace_back(1, 2, 3);
+	vertices.emplace_back( 4, 5, 6);
+	vertices.emplace_back(7, 8, 9);
+
 	std::cin.get();
 }
